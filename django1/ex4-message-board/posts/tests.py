7c6e73853bfbe0687504c8891b7927316a2f7e81
vijay more <more.vijay@gmail.com>
2024-04-03 00:54:47 +0530

    ex4 message-board-app added tests

--- django1/ex4-message-board/posts/tests.py
+++ django1/ex4-message-board/posts/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
+from .models import Post
+
 
 # Create your tests here.
+class AllUrlsCheck(TestCase):
+    validUrlPaths = ["/"]
+    validViewNames = ["home"]
+    templateViewTuples = [("home.html", "home")]
+
+    def test_urlpaths_exists_at_correct_location(self):
+        for urlpath in self.validUrlPaths:
+            self.assertEqual(self.client.get(urlpath).status_code, 200)
+
+    def test_urlpath_available_by_viewname(self):
+        for viewName in self.validViewNames:
+            self.assertEqual(self.client.get(reverse(viewName)).status_code, 200)
+
+    def test_correct_template_used_by_view(self):
+        for template, view in self.templateViewTuples:
+            self.assertTemplateUsed(self.client.get(reverse(view)), template)
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.post = Post.objects.create(text="This is a Test")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a Test")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "This is a Test")
