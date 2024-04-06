a30b9668d602b56d42fdab006be2514a9cc7c9c7
vijay more <more.vijay@gmail.com>
2024-04-06 23:46:54 +0530

    ex5 blog - forms - update delete

--- django1/ex5-blog/blog/views.py
+++ django1/ex5-blog/blog/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
 
@@ -17,3 +20,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
